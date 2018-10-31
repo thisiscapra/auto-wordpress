57fdaccf4c92519706ef92fb7624fb20646a82a9
Ollie Kavanagh <olliek@gmail.com>
2018-10-31 08:39:34 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
       dev: {
         options: {
           implementation: sass,
-          compass: true
+          style: 'compressed'
         },
         files: [
           {
